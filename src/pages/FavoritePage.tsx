85fe9e59c7a27676c4e20e3b543bb3c80fc71f5e
Tomáš Holý <holytom5@fel.cvut.cz>
2025-05-26 23:54:01 +0200

    adding favorite page

---
+++ src/pages/FavoritePage.tsx
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+function FavoritePage() {
+    return (
+        <Layout>
+            <h1>Oblíbené</h1>
+        </Layout>
+    )
+}
+
+export default FavoritePage
