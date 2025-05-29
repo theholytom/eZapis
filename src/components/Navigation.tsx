52ed7d22513a6dd72dcae88a37d0d9ecec280e81
Tomáš Holý <holytom5@fel.cvut.cz>
2025-05-29 13:21:18 +0200

    adding login and register links to navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,7 +4,7 @@ import cvLogo from "../assets/cesky-volejbal.png";
 function Navigation() {
     return (
         <nav className="fixed top-0 left-0 right-0 bg-primary p-4">
-            <ul className="flex items-center gap-8 tracking-wide [&>li_*]:(font-medium text-3xl text-white/90 transition-all duration-300)">
+            <ul className="flex items-center gap-8 tracking-wide [&>li_*]:(font-medium text-2xl text-white/90 transition-all duration-300)">
                 <li>
                     <a href="https://www.cvf.cz" className="flex items-center">
                         <img
@@ -53,6 +53,22 @@ function Navigation() {
                             Zápasy
                         </Link>
                     }
+                </li>
+                <li className="items-center ml-auto">
+                    <Link
+                        to="/login"
+                        className="relative after:absolute after:bg-white after:h-[2px] after:w-0 after:-bottom-1 after:left-0 hover:after:w-full after:transition-all after:duration-300 hover:text-white hover:translate-y-[-2px] rounded-md px-2 py-1"
+                    >
+                        Přihlásit se
+                    </Link>
+                </li>
+                <li className="items-center">
+                    <Link
+                        to="/register"
+                        className="relative after:absolute after:bg-white after:h-[2px] after:w-0 after:-bottom-1 after:left-0 hover:after:w-full after:transition-all after:duration-300 hover:text-white hover:translate-y-[-2px] rounded-md px-2 py-1"
+                    >
+                        Registrovat se
+                    </Link>
                 </li>
             </ul>
         </nav>
