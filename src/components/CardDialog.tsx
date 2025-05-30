aeea25c75455bd534afbf86aae16d0c8eb078070
Tomáš Holý <holytom5@fel.cvut.cz>
2025-05-30 12:52:26 +0200

    handling selected value

--- src/components/CardDialog.tsx
+++ src/components/CardDialog.tsx
@@ -12,20 +12,44 @@ import {
 import { buttonVariants } from "@/components/ui/button";
 import type { Player } from "@/types/player";
 import PlayerSelect from "./PlayerSelect";
+import { useState } from "react";
 
 interface Props {
     card: string;
     players: Player[];
     buttonText: string;
     buttonClass?: string;
-    disabled: boolean
+    disabled: boolean;
+    onCardGiven: (playerId: string) => void;
 }
 
-function CardDialog({ card, players, buttonText, buttonClass, disabled }: Props) {
+function CardDialog({
+    card,
+    players,
+    buttonText,
+    buttonClass,
+    disabled,
+    onCardGiven,
+}: Props) {
+    const [playerSelect, setPlayerSelect] = useState("");
+
+    const handleSelect = (playerId: string) => {
+        setPlayerSelect(playerId);
+    };
+
+    const handleCardConfirm = () => {
+        if (playerSelect) {
+            onCardGiven(playerSelect);
+        }
+    };
+
     return (
         <AlertDialog>
             <AlertDialogTrigger asChild>
-                <button disabled={disabled} className={buttonVariants({ className: buttonClass })}>
+                <button
+                    disabled={disabled}
+                    className={buttonVariants({ className: buttonClass })}
+                >
                     {buttonText}
                 </button>
             </AlertDialogTrigger>
@@ -35,12 +59,15 @@ function CardDialog({ card, players, buttonText, buttonClass, disabled }: Props)
                         Komu je {card} karta udělena?
                     </AlertDialogTitle>
                     <AlertDialogDescription>
-                        <PlayerSelect players={players} />
+                        <PlayerSelect
+                            onSelect={handleSelect}
+                            players={players}
+                        />
                     </AlertDialogDescription>
                 </AlertDialogHeader>
                 <AlertDialogFooter>
                     <AlertDialogCancel>Zrušit</AlertDialogCancel>
-                    <AlertDialogAction>
+                    <AlertDialogAction onClick={handleCardConfirm}>
                         <button className="font-bold bg-primary-foreground text-primary w-[110px] hover:bg-secondary hover:text-primary-foreground inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm h-9 px-4 py-2">
                             Udělit Kartu
                         </button>
