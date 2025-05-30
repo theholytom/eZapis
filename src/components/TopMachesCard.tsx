89ced31b5628b8821d842ace280e6b6d9fa90a62
Tomáš Holý <holytom5@fel.cvut.cz>
2025-05-30 16:35:29 +0200

    editing matchCards to accept matchId only

--- src/components/TopMachesCard.tsx
+++ src/components/TopMachesCard.tsx
@@ -3,27 +3,21 @@ import MatchCard from "./ui/MatchCard";
 function TopMatchesCard() {
     return (
         <div className="flex-col space-y-5 p-5">
-            <h2 className="text-3xl">Top Zápasy dne</h2>
+            <h2 className="text-3xl">Top Zápasy</h2>
             <ul className="flex flex-row gap-4">
                 <li>
                     <MatchCard
-                        team1="Karlovarsko"
-                        team2="Lvi Praha"
-                        time={new Date(2025, 11, 24, 19, 0)}
+                        matchId="11"
                     />
                 </li>
                 <li>
                     <MatchCard
-                        team1="Kladno"
-                        team2="Hradec Králové"
-                        time={new Date(2025, 11, 24, 19, 0)}
+                        matchId="19"
                     />
                 </li>
                 <li>
                     <MatchCard
-                        team1="Karlovarsko"
-                        team2="Lvi Praha"
-                        time={new Date(2025, 11, 24, 19, 0)}
+                        matchId="21"
                     />
                 </li>
             </ul>
--- src/components/ui/MatchCard.tsx
+++ src/components/ui/MatchCard.tsx
@@ -1,25 +1,28 @@
-import {
-    Card,
-    CardContent,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { matches } from "@/data/matches.json";
+import { teams } from "@/data/teams";
+import { Link } from "react-router-dom";
 
 interface MatchCardProps {
-    team1: string;
-    team2: string;
-    time: Date;
+    matchId: string;
 }
 
-export default function MatchCard({ team1, team2, time }: MatchCardProps) {
+export default function MatchCard({ matchId }: MatchCardProps) {
+    const match = matches.find((m) => m.id === matchId);
+    const team1 = teams.find((t) => t.id === match?.firstTeamId)?.name || "";
+    const team2 = teams.find((t) => t.id === match?.secondTeamId)?.name || "";
+    const time = match ? new Date(match.date) : new Date();
+
     return (
-        <Card className="bg-chart-1 text-primary-foreground flex-column min-w-[300px]">
-            <CardHeader>
-                <CardTitle className="whitespace-nowrap">
-                    {team1} vs {team2}
-                </CardTitle>
-            </CardHeader>
-            <CardContent>{time.toLocaleString()}</CardContent>
+        <Card className="bg-chart-1 text-primary-foreground flex-column w-[500px]">
+            <Link to={`/matches/${matchId}`}>
+                <CardHeader>
+                    <CardTitle className="whitespace-nowrap">
+                        {team1} vs {team2}
+                    </CardTitle>
+                </CardHeader>
+                <CardContent>{time.toLocaleString()}</CardContent>
+            </Link>
         </Card>
     );
 }
